9d0fa2bd27597d70228327e59837abe5ac88399c
Kirara17233 <Kirara17233@gmail.com>
2021-07-07 18:01:10 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendfile('public/index.html');
+});
+
+router.get('/list', function(req, res, next) {
+  res.sendfile('public/list.html');
 });
 
 module.exports = router;
